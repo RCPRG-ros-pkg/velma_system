fa8f8e7ef4073a12f51e47620fb9fd9bb3f620c4
Dawid Seredyński <dawid.seredynski@gmail.com>
2016-01-18 15:54:09 +0100

    disabled collisions from srdf support

--- src/velma_gazebo.cpp
+++ src/velma_gazebo.cpp
@@ -38,6 +38,7 @@ protected:
     gazebo::physics::ModelPtr model_;
     gazebo::physics::DARTModelPtr model_dart_;
     dart::dynamics::Skeleton *dart_sk_;
+    dart::simulation::World *dart_world_;
 
     std::vector<gazebo::physics::JointPtr>  r_joints_;
     std::vector<gazebo::physics::JointPtr>  l_joints_;
@@ -105,6 +106,7 @@ public:
     Eigen::VectorXd t_JointVelocity_out_;
 
     int counts_;
+    int counts2_;
 
     VelmaGazebo(std::string const& name) : 
         TaskContext(name)
@@ -122,6 +124,7 @@ public:
         // Add required gazebo interfaces
         this->provides("gazebo")->addOperation("configure",&VelmaGazebo::gazeboConfigureHook,this,RTT::ClientThread);
         this->provides("gazebo")->addOperation("update",&VelmaGazebo::gazeboUpdateHook,this,RTT::ClientThread);
+//        this->provides("gazebo")->addOperation("exit",&VelmaGazebo::gazeboExit,this,RTT::ClientThread);
 
         // right KUKA FRI ports
         this->ports()->addPort("r_JointTorqueCommand",        port_r_JointTorqueCommand_in_).doc("");
@@ -182,10 +185,80 @@ public:
     ~VelmaGazebo() {
     }
 
+    bool parseDisableCollision(std::string &link1, std::string &link2, TiXmlElement *c)
+    {
+        const char* link1_str = c->Attribute("link1");
+        const char* link2_str = c->Attribute("link2");
+        if (link1_str != NULL && link2_str != NULL)
+        {
+            link1 = link1_str;
+            link2 = link2_str;
+            return true;
+        }
+        std::cout<< "disable_collisions has wrong attributes" << std::endl;
+
+        return false;
+    }
+
+    bool parseSRDF(const std::string &xml_string, std::vector<std::pair<std::string, std::string> > &disabled_collisions)
+    {
+        disabled_collisions.clear();
+
+        TiXmlDocument xml_doc;
+        xml_doc.Parse(xml_string.c_str());
+        if (xml_doc.Error())
+        {
+//            ROS_ERROR("%s", xml_doc.ErrorDesc());
+            xml_doc.ClearError();
+            return false;
+        }
+
+        TiXmlElement *robot_xml = xml_doc.FirstChildElement("robot");
+        if (!robot_xml)
+        {
+//            ROS_ERROR("Could not find the 'robot' element in the xml file");
+            return false;
+        }
+        // Get robot name
+/*        const char *name = robot_xml->Attribute("name");
+        if (!name)
+        {
+//            ROS_ERROR("No name given for the robot.");
+            return false;
+        }
+
+        if (name_ != std::string(name))
+        {
+//            ROS_ERROR("Name from SRDF: %s differ from %s", name, name_.c_str());
+            return false;
+        }
+*/
+        // Get all disable_collisions elements
+        for (TiXmlElement* disable_collision_xml = robot_xml->FirstChildElement("disable_collisions"); disable_collision_xml; disable_collision_xml = disable_collision_xml->NextSiblingElement("disable_collisions"))
+        {
+            std::string link1, link2;
+            try {
+                if (!parseDisableCollision(link1, link2, disable_collision_xml)) {
+                    return false;
+                }
+                disabled_collisions.push_back(std::make_pair(link1, link2));
+            }
+            catch (urdf::ParseError &e) {
+//                ROS_ERROR("disable_collisions xml is not initialized correctly");
+                return false;
+            }
+        }
+        return true;
+    }
+
     void updateHook() {
         // Synchronize with gazeboUpdate()
         RTT::os::MutexLock lock(gazebo_mutex_);
         counts_ = 0;
+
+        counts2_++;
+//        std::cout << "c2: " << counts2_ << std::endl;
+
 
         port_r_MassMatrix_out_.write(r_MassMatrix_out_);
         port_l_MassMatrix_out_.write(l_MassMatrix_out_);
@@ -241,6 +314,7 @@ public:
 //        std::cout << "VelmaGazebo::configureHook: property2: " << prop_parameter << std::endl;
 
         counts_ = 0;
+        counts2_ = 0;
 
         if (!model_) {
             std::cout << "VelmaGazebo::configureHook: model_ is NULL" << std::endl;
@@ -266,6 +340,8 @@ public:
 
         model_ = model;
 
+        dart_world_ = boost::dynamic_pointer_cast < gazebo::physics::DARTPhysics > ( gazebo::physics::get_world()->GetPhysicsEngine() ) -> GetDARTWorld();
+
         model_dart_ = boost::dynamic_pointer_cast < gazebo::physics::DARTModel >(model);
         if (model_dart_.get() == NULL) {
             std::cout << "VelmaGazebo::gazeboConfigureHook: the gazebo model is not a DART model" << std::endl;
@@ -273,6 +349,20 @@ public:
         }
 
         dart_sk_ = model_dart_->GetDARTSkeleton();
+
+        std::string srdf;
+        ros::param::get("/robot_semantic_description", srdf);
+//        std::cout << srdf << std::endl;
+        dart::collision::CollisionDetector* detector = dart_world_->getConstraintSolver()->getCollisionDetector();
+
+        std::vector<std::pair<std::string, std::string> > disabled_collisions;
+        parseSRDF(srdf, disabled_collisions);
+        for (std::vector<std::pair<std::string, std::string> >::const_iterator it = disabled_collisions.begin(); it != disabled_collisions.end(); it++) {
+            dart::dynamics::BodyNode *bn1 = dart_sk_->getBodyNode(it->first);
+            dart::dynamics::BodyNode *bn2 = dart_sk_->getBodyNode(it->second);
+            detector->disablePair(bn1, bn2);
+        }
+        dart_sk_->enableSelfCollision(false);
 
         // fill in gazebo joints pointer vectors
         for(unsigned int i = 0; i < 7; i++) {
@@ -332,8 +422,29 @@ void gazeboUpdateHook(gazebo::physics::ModelPtr model)
         return;
     }
 
+    dart::collision::CollisionDetector* detector = dart_world_->getConstraintSolver()->getCollisionDetector();
+    detector->detectCollision(true, true);
+    size_t collisionCount = detector->getNumContacts();
+    if (collisionCount > 0) {
+        std::cout << "col: " << collisionCount << std::endl;
+        for(size_t i = 0; i < collisionCount; ++i)
+        {
+          const dart::collision::Contact& contact = detector->getContact(i);
+            std::cout << contact.bodyNode1->getName() << " " << contact.bodyNode2->getName() << std::endl;
+//          if(contact.bodyNode1.lock()->getSkeleton() == object
+//             || contact.bodyNode2.lock()->getSkeleton() == object)
+//          {
+//            collision = true;
+//            break;
+//          }
+        }
+
+    }
+
     counts_++;
-    std::cout << counts_ << std::endl;
+//    std::cout << "c1: " << counts_ << std::endl;
+
+    counts2_ = 0;
 
     // mass matrix
     const Eigen::MatrixXd &mass_matrix = dart_sk_->getMassMatrix();
@@ -407,35 +518,7 @@ void gazeboUpdateHook(gazebo::physics::ModelPtr model)
     //! Synchronization
     RTT::os::MutexRecursive gazebo_mutex_;
 
-    ros::NodeHandle* rosnode_;
-    
-    std::vector<gazebo::physics::JointPtr>  joints_;
-    std::string chain_start, chain_end;
-    
-    
-    KDL::Chain chain_;
-    KDL::ChainDynParam *dyn;
-    KDL::ChainFkSolverPos_recursive *fk;
-    KDL::ChainJntToJacSolver *jc;
-
-    std::string base_frame_;
-
-    Eigen::Matrix<double, 7, 1> joint_pos_;
-    Eigen::Matrix<double, 7, 1> joint_pos_cmd_;
-    Eigen::Matrix<double, 7, 1> joint_vel_;
-    Eigen::Matrix<double, 7, 1> stiffness_;
-    Eigen::Matrix<double, 7, 1> damping_;
-    Eigen::Matrix<double, 7, 1> trq_cmd_;
-    Eigen::Matrix<double, 7, 1> trq_;
-    
-    std::vector<double> jnt_pos_;
-    std::vector<double> jnt_trq_;
-    std::vector<double> jnt_vel_;
-
-    Eigen::VectorXd jnt_pos_cmd_;
-    Eigen::VectorXd jnt_trq_cmd_;
-
-    KDL::Frame T_old;
+//    std::vector<gazebo::physics::JointPtr>  joints_;
 };
 
 
