337ee8cf0cd8d11e210daa039e7a953166af3103
Dawid Seredyński <dawid.seredynski@gmail.com>
2016-01-20 16:23:03 +0100

    grippers

--- src/velma_gazebo.cpp
+++ src/velma_gazebo.cpp
@@ -56,6 +56,8 @@ protected:
     bool r_command_mode_;
     bool l_command_mode_;
 
+    gazebo::physics::JointController *jc_;
+
 public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
@@ -476,7 +478,7 @@ public:
     bool configureHook() {
         counts_ = 0;
         counts2_ = 0;
-
+/*
         if (!model_) {
             std::cout << "VelmaGazebo::configureHook: model_ is NULL" << std::endl;
             return false;
@@ -486,8 +488,13 @@ public:
             r_joints_[i]->SetPosition(0, 0.3);
             l_joints_[i]->SetPosition(0, 0.3);
         }
-        model_->ResetPhysicsStates();
 
+        for (int i = 0; i < 8; i++) {
+            rh_joints_[i]->SetPosition(0, 0.3);
+            lh_joints_[i]->SetPosition(0, 0.3);
+        }
+        model_->ResetPhysicsStates();
+*/
         std::cout << "VelmaGazebo::configureHook: ok" << std::endl;
         return true;
     }
@@ -515,23 +522,40 @@ public:
             "_HandFingerTwoKnuckleOneJoint", "_HandFingerTwoKnuckleTwoJoint", "_HandFingerTwoKnuckleThreeJoint",
             "_HandFingerThreeKnuckleTwoJoint", "_HandFingerThreeKnuckleThreeJoint" };
 
+        jc_ = new gazebo::physics::JointController(model);
         for (int i = 0; i < 8; i++) {
             std::string rh_name( std::string("right") + hand_joint_names[i] );
             std::string lh_name( std::string("left") + hand_joint_names[i] );
-            dart_sk_->getJoint(rh_name)->setActuatorType( dart::dynamics::Joint::SERVO );
-            dart_sk_->getJoint(lh_name)->setActuatorType( dart::dynamics::Joint::SERVO );
+            dart_sk_->getJoint(rh_name)->setActuatorType( dart::dynamics::Joint::FORCE );
+            dart_sk_->getJoint(lh_name)->setActuatorType( dart::dynamics::Joint::FORCE );
             gazebo::physics::JointPtr rh_joint = model->GetJoint(rh_name);
             if (rh_joint) {
                 rh_joints_.push_back(rh_joint);
                 rh_joints_dart_.push_back( dart_sk_->getJoint(rh_name) );
             }
+            gazebo::common::PID pid0(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+            gazebo::common::PID pid(10.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0);//300.0, -300.0);
+            jc_->AddJoint(rh_joint);
+//            jc_->SetPositionPID(rh_joint->GetScopedName(), pid);
+//            jc_->SetVelocityPID(rh_joint->GetScopedName(), pid);
 
             gazebo::physics::JointPtr lh_joint = model->GetJoint(lh_name);
             if (lh_joint) {
                 lh_joints_.push_back(lh_joint);
                 lh_joints_dart_.push_back( dart_sk_->getJoint(lh_name) );
             }
+            jc_->AddJoint(lh_joint);
+//            jc_->SetPositionPID(lh_joint->GetScopedName(), pid);
+//            jc_->SetVelocityPID(lh_joint->GetScopedName(), pid);
         }
+
+//        std::cout << rh_joints_[0]->GetScopedName() << std::endl;
+
+//        std::map< std::string,gazebo::common::PID > map = jc_->GetVelocityPIDs();
+//        for (std::map< std::string,gazebo::common::PID >::const_iterator it = map.begin(); it != map.end(); it++) {
+//            std::cout << "PID map: " << it->first << std::endl;
+//        }
+
 
         std::string srdf;
         ros::param::get("/robot_semantic_description", srdf);
@@ -586,6 +610,24 @@ public:
                 return false;
             }
         }
+
+        for (int i = 0; i < 7; i++) {
+            r_joints_[i]->SetPosition(0, 0.3);
+            l_joints_[i]->SetPosition(0, 0.3);
+        }
+
+        for (int i = 0; i < 8; i++) {
+            rh_joints_[i]->SetPosition(0, 0.3);
+            lh_joints_[i]->SetPosition(0, 0.3);
+        }
+        std::cout << "pos limits: " << rh_joints_dart_[0]->getPositionLowerLimit(0) << "  " << rh_joints_dart_[0]->getPositionUpperLimit(0) << std::endl;
+        std::cout << "pos: " << rh_joints_dart_[0]->getPosition(0) << std::endl;
+
+        rh_joints_dart_[0]->setPositionLowerLimit(0, -0.1);
+        model_->ResetPhysicsStates();
+
+        std::cout << "pos limits: " << rh_joints_dart_[0]->getPositionLowerLimit(0) << "  " << rh_joints_dart_[0]->getPositionUpperLimit(0) << std::endl;
+        std::cout << "pos: " << rh_joints_dart_[0]->getPosition(0) << std::endl;
 
         return true;
     }
@@ -647,8 +689,21 @@ void gazeboUpdateHook(gazebo::physics::ModelPtr model)
     // external forces
     //const Eigen::VectorXd &ext_f = dart_sk_->getExternalForces();
     for (int i = 0; i < 7; i++) {
-        r_JointTorque_out_(i) = r_joints_[i]->GetForce(0); //ext_f[r_indices_[i]];
-        l_JointTorque_out_(i) = l_joints_[i]->GetForce(0); //ext_f[l_indices_[i]];
+
+        gazebo::physics::JointWrench r_wr, l_wr;
+        r_wr = r_joints_[i]->GetForceTorque(0);
+        l_wr = l_joints_[i]->GetForceTorque(0);
+        if (i%2 == 0) {
+            r_JointTorque_out_(i) = r_wr.body2Torque.z;
+            l_JointTorque_out_(i) = l_wr.body2Torque.z;
+        }
+        else {
+            r_JointTorque_out_(i) = r_wr.body2Torque.y;
+            l_JointTorque_out_(i) = l_wr.body2Torque.y;
+        }
+
+//        r_JointTorque_out_(i) = r_joints_[i]->GetForce(0); //ext_f[r_indices_[i]];
+//        l_JointTorque_out_(i) = l_joints_[i]->GetForce(0); //ext_f[l_indices_[i]];
     }
 
     // joint position
@@ -672,75 +727,240 @@ void gazeboUpdateHook(gazebo::physics::ModelPtr model)
     // torque command
     if (r_command_mode_) {
         for (int i = 0; i < 7; i++) {
+//            r_joints_[i]->SetForce(0, r_JointTorqueCommand_in_(i));
             r_joints_[i]->SetForce(0, r_JointTorqueCommand_in_(i));
         }
-//        r_JointTorqueCommand_in_.setZero();
+        r_JointTorqueCommand_in_.setZero();
     }
 
     if (l_command_mode_) {
         for (int i = 0; i < 7; i++) {
             l_joints_[i]->SetForce(0, l_JointTorqueCommand_in_(i));
         }
-//        l_JointTorqueCommand_in_.setZero();
+        l_JointTorqueCommand_in_.setZero();
     }
 
     for (int i = 0; i < t_joints_.size(); i++) {
         t_joints_[i]->SetForce(0, t_JointTorqueCommand_in_(i));
     }
+    t_JointTorqueCommand_in_.setZero();
 
     //
     // BarrettHand
     //
 
+    const double force_factor = 1000.0;
     // joint position
     for (int i = 0; i < 8; i++) {
         rh_q_out_(i) = rh_joints_[i]->GetAngle(0).Radian();
         lh_q_out_(i) = lh_joints_[i]->GetAngle(0).Radian();
     }
 
-    rh_t_out_[0] = rh_t_out_[3] = rh_joints_[0]->GetForce(0)*100.0;
-    rh_t_out_[1] = rh_t_out_[2] = rh_joints_[1]->GetForce(0)*100.0;
-    rh_t_out_[4] = rh_t_out_[5] = rh_joints_[4]->GetForce(0)*100.0;
-    rh_t_out_[6] = rh_t_out_[7] = rh_joints_[6]->GetForce(0)*100.0;
+    rh_t_out_[0] = rh_t_out_[3] = rh_joints_[0]->GetForce(0)*force_factor;
+    rh_t_out_[1] = rh_t_out_[2] = rh_joints_[1]->GetForce(0)*force_factor;
+    rh_t_out_[4] = rh_t_out_[5] = rh_joints_[4]->GetForce(0)*force_factor;
+    rh_t_out_[6] = rh_t_out_[7] = rh_joints_[6]->GetForce(0)*force_factor;
 
-    lh_t_out_[0] = lh_t_out_[3] = lh_joints_[0]->GetForce(0)*100.0;
-    lh_t_out_[1] = lh_t_out_[2] = lh_joints_[1]->GetForce(0)*100.0;
-    lh_t_out_[4] = lh_t_out_[5] = lh_joints_[4]->GetForce(0)*100.0;
-    lh_t_out_[6] = lh_t_out_[7] = lh_joints_[6]->GetForce(0)*100.0;
+    lh_t_out_[0] = lh_t_out_[3] = lh_joints_[0]->GetForce(0)*force_factor;
+    lh_t_out_[1] = lh_t_out_[2] = lh_joints_[1]->GetForce(0)*force_factor;
+    lh_t_out_[4] = lh_t_out_[5] = lh_joints_[4]->GetForce(0)*force_factor;
+    lh_t_out_[6] = lh_t_out_[7] = lh_joints_[6]->GetForce(0)*force_factor;
 
+    // spread joints
     const double vel_trap_angle = 5.0/180.0*3.1415;
 
-    double diff;
-    diff = rh_q_in_[0] - rh_joints_[0]->GetAngle(0).Radian();
+    double diff, force;
+    int dof_idx, jnt_idx;
+    dof_idx = 3;
+    jnt_idx = 0;
+    diff = rh_q_in_[dof_idx] - rh_joints_[jnt_idx]->GetAngle(0).Radian();
     if (diff > vel_trap_angle) {
         diff = vel_trap_angle;
     }
     if (diff < -vel_trap_angle) {
         diff = -vel_trap_angle;
     }
-//    rh_joints_dart_[0]->setForceUpperLimit(0, rh_t_in_[0]/100.0);
-//    rh_joints_dart_[0]->setForceLowerLimit(0, -rh_t_in_[0]/100.0);
-    std::cout << "cmd: " << rh_v_in_[0] * diff / vel_trap_angle << std::endl;
-    rh_joints_dart_[0]->setCommand(0, rh_v_in_[0] * diff / vel_trap_angle);
+    force = rh_joints_dart_[jnt_idx]->getForce(0);
+//    std::cout << force << std::endl;
+/*    if (force > rh_t_in_[dof_idx]/force_factor || force < -rh_t_in_[dof_idx]/force_factor) {
+        if (!jc_->SetVelocityTarget(rh_joints_[jnt_idx]->GetScopedName(), 0)) {
+            std::cout << "joint " << rh_joints_[jnt_idx]->GetScopedName() << " not found" << std::endl;
+        }
+        jc_->SetPositionTarget(rh_joints_[jnt_idx]->GetScopedName(), rh_joints_[jnt_idx]->GetAngle(0).Radian());
+//        rh_joints_dart_[jnt_idx]->setCommand(0, 0);
+    }
+    else {
+        if (!jc_->SetVelocityTarget(rh_joints_[jnt_idx]->GetScopedName(), rh_v_in_[dof_idx] * diff / vel_trap_angle)) {
+            std::cout << "joint " << rh_joints_[jnt_idx]->GetScopedName() << " not found" << std::endl;
+        }
+        jc_->SetPositionTarget(rh_joints_[jnt_idx]->GetScopedName(), rh_q_in_[dof_idx]);
+//        rh_joints_dart_[jnt_idx]->setCommand(0, rh_v_in_[dof_idx] * diff / vel_trap_angle);
+    }
+*/
 
-    diff = rh_q_in_[3] - rh_joints_[3]->GetAngle(0).Radian();
+    jc_->SetVelocityTarget(rh_joints_[0]->GetScopedName(), 1.0);
+//    if (!jc_->SetPositionTarget(rh_joints_[3]->GetScopedName(), -3.5)) {
+//        std::cout << "joint " << rh_joints_[3]->GetScopedName() << " not found" << std::endl;
+//    }
+/*    jc_->SetVelocityTarget(rh_joints_[1]->GetScopedName(), 0);
+    jc_->SetPositionTarget(rh_joints_[1]->GetScopedName(), 0);
+    jc_->SetVelocityTarget(rh_joints_[2]->GetScopedName(), 0);
+    jc_->SetPositionTarget(rh_joints_[2]->GetScopedName(), 0);
+    jc_->SetVelocityTarget(rh_joints_[3]->GetScopedName(), 0);
+    jc_->SetPositionTarget(rh_joints_[3]->GetScopedName(), 0);
+    jc_->SetVelocityTarget(rh_joints_[4]->GetScopedName(), 0);
+    jc_->SetPositionTarget(rh_joints_[4]->GetScopedName(), 0);
+    jc_->SetVelocityTarget(rh_joints_[5]->GetScopedName(), 0);
+    jc_->SetPositionTarget(rh_joints_[5]->GetScopedName(), 0);
+    jc_->SetVelocityTarget(rh_joints_[6]->GetScopedName(), 0);
+    jc_->SetPositionTarget(rh_joints_[6]->GetScopedName(), 0);
+    jc_->SetVelocityTarget(rh_joints_[7]->GetScopedName(), 0);
+    jc_->SetPositionTarget(rh_joints_[7]->GetScopedName(), 0);
+*/
+    jc_->Update();
+/*
+    double diff, force;
+    int dof_idx, jnt_idx;
+    dof_idx = 3;
+    jnt_idx = 0;
+    diff = rh_q_in_[dof_idx] - rh_joints_[jnt_idx]->GetAngle(0).Radian();
     if (diff > vel_trap_angle) {
         diff = vel_trap_angle;
     }
     if (diff < -vel_trap_angle) {
         diff = -vel_trap_angle;
     }
-//    rh_joints_dart_[3]->setForceUpperLimit(0, rh_t_in_[3]/100.0);
-//    rh_joints_dart_[3]->setForceLowerLimit(0, -rh_t_in_[3]/100.0);
-    rh_joints_dart_[3]->setCommand(0, rh_v_in_[3] * diff / vel_trap_angle);
+    force = rh_joints_dart_[jnt_idx]->getForce(0);
+    if (force > rh_t_in_[dof_idx]/force_factor || force < -rh_t_in_[dof_idx]/force_factor) {
+        rh_joints_dart_[jnt_idx]->setCommand(0, 0);
+    }
+    else {
+        rh_joints_dart_[jnt_idx]->setCommand(0, rh_v_in_[dof_idx] * diff / vel_trap_angle);
+    }
 
+    dof_idx = 3;
+    jnt_idx = 3;
+    diff = rh_q_in_[dof_idx] - rh_joints_[jnt_idx]->GetAngle(0).Radian();
+    if (diff > vel_trap_angle) {
+        diff = vel_trap_angle;
+    }
+    if (diff < -vel_trap_angle) {
+        diff = -vel_trap_angle;
+    }
+    force = rh_joints_dart_[jnt_idx]->getForce(0);
+    if (force > rh_t_in_[dof_idx]/force_factor || force < -rh_t_in_[dof_idx]/force_factor) {
+        rh_joints_dart_[jnt_idx]->setCommand(0, 0);
+    }
+    else {
+        rh_joints_dart_[jnt_idx]->setCommand(0, rh_v_in_[dof_idx] * diff / vel_trap_angle);
+    }
 
-//    r_JointTorqueCommand_in_.setZero();
-//    l_JointTorqueCommand_in_.setZero();
-//    t_JointTorqueCommand_in_.setZero();
+    // f1 joints
+    dof_idx = 0;
+    jnt_idx = 1;
+    diff = rh_q_in_[dof_idx] - rh_joints_[jnt_idx]->GetAngle(0).Radian();
+    if (diff > vel_trap_angle) {
+        diff = vel_trap_angle;
+    }
+    if (diff < -vel_trap_angle) {
+        diff = -vel_trap_angle;
+    }
+    force = rh_joints_dart_[jnt_idx]->getForce(0);
+    if (force > rh_t_in_[dof_idx]/force_factor || force < -rh_t_in_[dof_idx]/force_factor) {
+        rh_joints_dart_[jnt_idx]->setCommand(0, 0);
+    }
+    else {
+        rh_joints_dart_[jnt_idx]->setCommand(0, rh_v_in_[dof_idx] * diff / vel_trap_angle);
+    }
 
+    dof_idx = 0;
+    jnt_idx = 2;
+    diff = rh_q_in_[dof_idx]*0.3333333 - rh_joints_[jnt_idx]->GetAngle(0).Radian();
+    if (diff > vel_trap_angle) {
+        diff = vel_trap_angle;
+    }
+    if (diff < -vel_trap_angle) {
+        diff = -vel_trap_angle;
+    }
+    force = rh_joints_dart_[jnt_idx]->getForce(0);
+    if (force > rh_t_in_[dof_idx]/force_factor || force < -rh_t_in_[dof_idx]/force_factor) {
+        rh_joints_dart_[jnt_idx]->setCommand(0, 0);
+    }
+    else {
+        rh_joints_dart_[jnt_idx]->setCommand(0, rh_v_in_[dof_idx] * diff / vel_trap_angle);
+    }
+
+    // f2 joints
+    dof_idx = 1;
+    jnt_idx = 4;
+    diff = rh_q_in_[dof_idx] - rh_joints_[jnt_idx]->GetAngle(0).Radian();
+    if (diff > vel_trap_angle) {
+        diff = vel_trap_angle;
+    }
+    if (diff < -vel_trap_angle) {
+        diff = -vel_trap_angle;
+    }
+    force = rh_joints_dart_[jnt_idx]->getForce(0);
+    if (force > rh_t_in_[dof_idx]/force_factor || force < -rh_t_in_[dof_idx]/force_factor) {
+        rh_joints_dart_[jnt_idx]->setCommand(0, 0);
+    }
+    else {
+        rh_joints_dart_[jnt_idx]->setCommand(0, rh_v_in_[dof_idx] * diff / vel_trap_angle);
+    }
+
+    dof_idx = 1;
+    jnt_idx = 5;
+    diff = rh_q_in_[dof_idx]*0.3333333 - rh_joints_[jnt_idx]->GetAngle(0).Radian();
+    if (diff > vel_trap_angle) {
+        diff = vel_trap_angle;
+    }
+    if (diff < -vel_trap_angle) {
+        diff = -vel_trap_angle;
+    }
+    force = rh_joints_dart_[jnt_idx]->getForce(0);
+    if (force > rh_t_in_[dof_idx]/force_factor || force < -rh_t_in_[dof_idx]/force_factor) {
+        rh_joints_dart_[jnt_idx]->setCommand(0, 0);
+    }
+    else {
+        rh_joints_dart_[jnt_idx]->setCommand(0, rh_v_in_[dof_idx] * diff / vel_trap_angle);
+    }
+
+    // f3 joints
+    dof_idx = 2;
+    jnt_idx = 6;
+    diff = rh_q_in_[dof_idx] - rh_joints_[jnt_idx]->GetAngle(0).Radian();
+    if (diff > vel_trap_angle) {
+        diff = vel_trap_angle;
+    }
+    if (diff < -vel_trap_angle) {
+        diff = -vel_trap_angle;
+    }
+    force = rh_joints_dart_[jnt_idx]->getForce(0);
+    if (force > rh_t_in_[dof_idx]/force_factor || force < -rh_t_in_[dof_idx]/force_factor) {
+        rh_joints_dart_[jnt_idx]->setCommand(0, 0);
+    }
+    else {
+        rh_joints_dart_[jnt_idx]->setCommand(0, rh_v_in_[dof_idx] * diff / vel_trap_angle);
+    }
+
+    dof_idx = 2;
+    jnt_idx = 7;
+    diff = rh_q_in_[dof_idx]*0.3333333 - rh_joints_[jnt_idx]->GetAngle(0).Radian();
+    if (diff > vel_trap_angle) {
+        diff = vel_trap_angle;
+    }
+    if (diff < -vel_trap_angle) {
+        diff = -vel_trap_angle;
+    }
+    force = rh_joints_dart_[jnt_idx]->getForce(0);
+    if (force > rh_t_in_[dof_idx]/force_factor || force < -rh_t_in_[dof_idx]/force_factor) {
+        rh_joints_dart_[jnt_idx]->setCommand(0, 0);
+    }
+    else {
+        rh_joints_dart_[jnt_idx]->setCommand(0, rh_v_in_[dof_idx] * diff / vel_trap_angle);
+    }
+*/
 // TODO:
-//    std_msgs::Int32       r_KRL_CMD_in_;
 //    geometry_msgs::Wrench r_CartesianWrench_out_;
 
 }
