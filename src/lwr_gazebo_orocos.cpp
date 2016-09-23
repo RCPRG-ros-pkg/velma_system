54fb0778b6417bf4d85755920b0e01836c86accf
Dawid Seredyński <dawid.seredynski@gmail.com>
2016-09-23 19:02:27 +0200

    code minor cleanup

--- src/lwr_gazebo_orocos.cpp
+++ src/lwr_gazebo_orocos.cpp
@@ -52,7 +52,7 @@ using namespace RTT;
         port_JointVelocity_out_.write(JointVelocity_out_);
 
         if (port_KRL_CMD_in_.read(KRL_CMD_in_) == RTT::NewData) {
-            if (KRL_CMD_in_.data == 1) {
+            if (1 == KRL_CMD_in_.data) {
                 if (!command_mode_) {
                     command_mode_ = true;
                     Logger::log() << Logger::Info <<  "switched to command mode" << Logger::endl;
@@ -61,7 +61,7 @@ using namespace RTT;
                     Logger::log() << Logger::Warning <<  "tried to switch to command mode while in command mode" << Logger::endl;
                 }
             }
-            else if (KRL_CMD_in_.data == 2) {
+            else if (2 == KRL_CMD_in_.data) {
                 if (command_mode_) {
                     command_mode_ = false;
                     Logger::log() << Logger::Info << "switched to monitor mode" << Logger::endl;
