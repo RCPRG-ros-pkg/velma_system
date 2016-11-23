00d7b84638822b59e2eb73f90bcaa0ddec26685e
Dawid Seredyński <dawid.seredynski@gmail.com>
2016-11-23 12:06:19 +0100

    added idle behavior for velma core cs

---
+++ src/state_idle.cpp
@@ -0,0 +1,66 @@
+/*
+ Copyright (c) 2014, Robot Control and Pattern Recognition Group, Warsaw University of Technology
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+     * Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+     * Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+     * Neither the name of the Warsaw University of Technology nor the
+       names of its contributors may be used to endorse or promote products
+       derived from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL <COPYright HOLDER> BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "common_behavior/abstract_state.h"
+
+#include "velma_core_cs_msgs/VelmaCoreCsCommand.h"
+#include "velma_low_level_interface_msgs/VelmaLowLevelStatus.h"
+
+using namespace velma_low_level_interface_msgs;
+using namespace velma_core_cs_msgs;
+
+namespace velma_core_cs_types {
+
+class StateIdle : public StateBase<VelmaLowLevelStatus, VelmaCoreCsCommand> {
+public:
+    typedef VelmaLowLevelStatus TYPE_BUF_LO;
+    typedef VelmaCoreCsCommand TYPE_BUF_HI;
+
+    StateIdle() :
+        StateBase("state_velma_core_cs_idle", "behavior_velma_core_cs_idle")
+    {
+    }
+
+    bool checkInitialCondition(
+                const TYPE_BUF_LO& buf_lo,
+                const TYPE_BUF_HI& buf_hi,
+                const std::vector<RTT::TaskContext*> &components,
+                const std::string& prev_state_name,
+                bool in_error) const
+    {
+        if (prev_state_name == "state_velma_core_cs_idle") {
+            return false;
+        }
+
+        return true;
+    }
+};
+
+};  // namespace velma_core_cs_types
+
+REGISTER_STATE( VelmaLowLevelStatus, VelmaCoreCsCommand, velma_core_cs_types::StateIdle );
+
