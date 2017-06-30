9c77635f12ee9f110af4e6282995c6705afbef8d
Dawid Seredyński <dawid.seredynski@gmail.com>
2017-06-30 16:20:12 +0200

    added rtt_subsystem

--- src/predicates.cpp
+++ src/predicates.cpp
@@ -57,7 +57,7 @@ bool veBodyInSafeState( const InputDataConstPtr& in_data, const std::vector<cons
 }
 
 bool veBodyStatusValid( const InputDataConstPtr& in_data, const std::vector<const RTT::TaskContext*> &components) {
-    return in_data->b_st.sc_valid && in_data->b_st.rArm_valid && in_data->b_st.lArm_valid && in_data->b_st.tMotor_valid && in_data->b_st.hpMotor_valid && in_data->b_st.htMotor_valid;
+    return in_data->b_st_valid && in_data->b_st.sc_valid && in_data->b_st.rArm_valid && in_data->b_st.lArm_valid && in_data->b_st.tMotor_valid && in_data->b_st.hpMotor_valid && in_data->b_st.htMotor_valid;
 }
 
 bool recvCartImpCmd( const InputDataConstPtr& in_data, const std::vector<const RTT::TaskContext*> &components) {
